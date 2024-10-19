37c82714f3aac865959946d47b55866b144a1327
void <void@voids-MacBook-Pro.local>
2024-10-20 00:39:59 +0200

    update

--- src/js/index.js
+++ src/js/index.js
@@ -31,5 +31,59 @@ import { createAuthenticatedClient } from "@interledger/open-payments";
     console.log(receivingWalletAddress);
 
     // 1. getting the grant from incoming payment
+    const incomingPaymentGrant = client.grant.request(
+        { url: receivingWalletAddress.authServer}, // resource one when getting the grant is the auth server
+
+        // then we define our access token
+        { access_token: {
+            access : [{
+                type : "incoming-payment", // type of resource. We have incoming-payment, quote, outgoing-payment
+                actions: ["create"], // because we want to create the incoming payment grant
+            }],
+
+        }},
+    );
+
+
+    console.log(incomingPaymentGrant); // the grant
+
+    // 2. Creating an incoming payment request (this happens on the side of the person who's about to get paid)
+    const incomingPayment = await client.incomingPayment.create(
+        // data set one:
+        // includes url of the receiving wallets RS and the access token value from the income grant
+        { url: receivingWalletAddress.resourceServer, // found from requesting receiving wallet
+        accessToken: incomingPaymentGrant.access_token.value}, // found in the access token from the grant
+
+        // data set two:
+        // wallet address id, the assetCode and assetScale
+        {
+            walletAddress: receivingWalletAddress.id,
+            incomingAmount: {
+                assetCode: receivingWalletAddress.assetCode, // the currency
+                assetScale: receivingWalletAddress.assetScale, // the decimal point
+                value: "1000", // we are sending 1000 cents. Combined with the scale 2, this will then be 10.00 usd (assetCode)
+                // thus Siza will get 10.00
+            },
+        },
+    );
+
+    console.log(incomingPayment)
+    /**
+     * The incoming payment will return a bunch things, including methods to pay the person receiving the money
+     */
+
+    // 3. getting grant for incoming quote, this happens on the senders side
+    const quoteGrant = await client.grant.request(
+        { url: sendingWalletAddress.authServer}, the // auth of the sender
+        {
+            access_token: {
+                [
+                    {
+                        type: "quote", // because we requesting the sender that we want to quote them
+
+                    }
+                ]
+        }
+    )
 
 })();
